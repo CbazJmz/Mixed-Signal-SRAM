8586cbdbf412e9821f205f912e4d026399278872
CbazJmz <iesebastianjimenez@gmail.com>
2026-01-27 18:17:38 -0600

    Modified sram_cell to add outputs

--- rtl/sram_cell.sv
+++ rtl/sram_cell.sv
@@ -1,7 +1,9 @@
 module sram_cell (
-    input  logic row,
+    input  logic row_wr,
     input  real bl_col,
-    input  real blb_col
+    input  real blb_col,
+	output real obl_col,
+	output real oblb_col
 );
     // NMOS and NOT Gates nodes
     real inv1;
@@ -11,5 +13,8 @@ module sram_cell (
     nmos nmos2(blb_col,inv2,row);
     not not1(inv2,inv1);
     not not2(inv1,inv2);
+	
+	assign obl_col = inv1;
+	assign oblb_col = inv2;
 
 endmodule
--- verif/sram_cell_tb.sv
+++ verif/sram_cell_tb.sv
@@ -2,8 +2,10 @@ module sram_cell_tb#(
     parameter COLS=1)();
 	
 	logic data_in;
-	logic rd_wr;
-	logic row;
+	logic rd;
+	logic wr;
+	logic row_wr;
+	logic row_rd;
 	logic preout;
 
 	real bl_col;
@@ -12,11 +14,15 @@ module sram_cell_tb#(
 	real blb_wr;
 	real bl_rd;
 	real blb_rd;
+	real obl_col;
+	real oblb_col;
 	
     sram_cell cell1(
-    .row (row),
+    .row_wr (row_wr),
     .bl_col (bl_col),
-    .blb_col(blb_col)
+    .blb_col(blb_col),
+	.obl_col (obl_col),
+    .oblb_col(oblb_col)
     );
 	
 	write_driver #(.COLS (COLS)) writed1 (
@@ -25,68 +31,62 @@ module sram_cell_tb#(
 	.blb_wr (blb_wr)
 	);
 	
-	precharge #(.COLS (COLS)) pre1 (
-	.rd_wr (rd_wr),
-	.bl_rd (bl_rd),
-	.blb_rd (blb_rd)
-	);
-	
 	sense_amp #(.COLS (COLS)) amp1 (
-	.bl_col (bl_col),
-	.blb_col (blb_col),
+	.bl_col (obl_col),
+	.blb_col (oblb_col),
 	.preout (preout)
 	);
 	
 	// Multiplexer for read or write operation
-	assign blb_col = rd_wr ? blb_rd : blb_wr;
-	assign bl_col = rd_wr ? bl_rd : bl_wr;
+	assign blb_col = wr ? blb_rd : blb_wr;
+	assign bl_col = wr ? bl_rd : bl_wr;
 
     // Initial procedural block that is executed at t=0
     // This starts a concurrent process
     initial begin
 	
 		data_in = 1'b0;
-		rd_wr = 1'b0;
-		row = 1'b0;
+		wr = 1'b0;
+		row_wr = 1'b0;
 		
 		#10ns;
 		
 		//Write 1 operation
 		
 		data_in = 1'b1;
-		rd_wr = 1'b0;
+		wr = 1'b0;
 		#10ns;
-		row = 1'b1;
+		row_wr = 1'b1;
 		#10ns;
-		row = 1'b0;
+		row_wr = 1'b0;
 		#10ns;
 		
 		//Read operation
 		
-		rd_wr = 1'b1;
+		wr = 1'b1;
 		#10ns;
-		row = 1'b1;
+		row_wr = 1'b1;
 		#10ns;
-		row = 1'b0;
+		row_wr = 1'b0;
 		#10ns;
 		
 		//Write 0 operation
 		
 		data_in = 1'b0;
-		rd_wr = 1'b0;
+		wr = 1'b0;
 		#10ns;
-		row = 1'b1;
+		row_wr = 1'b1;
 		#10ns;
-		row = 1'b0;
+		row_wr = 1'b0;
 		#10ns;
 		
 		//Read operation
 		
-		rd_wr = 1'b1;
+		wr = 1'b1;
 		#10ns;
-		row = 1'b1;
+		row_wr = 1'b1;
 		#10ns;
-		row = 1'b0;
+		row_wr = 1'b0;
 		#10ns;
 
         $finish;
