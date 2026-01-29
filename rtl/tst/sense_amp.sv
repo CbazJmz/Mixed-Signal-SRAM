56f7e42d600032be8cd2701563cf0f51be4ad8e8
CbazJmz <iesebastianjimenez@gmail.com>
2026-01-29 17:49:39 -0600

    Modified rtl files

--- rtl/tst/sense_amp.sv
+++ rtl/tst/sense_amp.sv
@@ -1,7 +1,7 @@
 module sense_amp#(
     parameter COLS=16)(
-    input  real [COLS-1:0] bl_col,      //BL line from memory array
-    input  real [COLS-1:0] blb_col,     //BLB line from memory array
+    input  wire [COLS-1:0] obl_col,      //BL line from memory array
+    input  wire [COLS-1:0] oblb_col,     //BLB line from memory array
     output logic [COLS-1:0] preout       //Output from diff amplifiers
 );
 
@@ -9,7 +9,7 @@ module sense_amp#(
     //Generate diff amplifiers array
     generate
         for(i=0;i<COLS;i++) begin: COL
-            assign preout [i] = bl_col [i] > blb_col [i] ? 1'b1 : 1'b0;
+            assign preout [i] = obl_col [i] > oblb_col [i] ? 1'b1 : 1'b0;
 
         end
     endgenerate
