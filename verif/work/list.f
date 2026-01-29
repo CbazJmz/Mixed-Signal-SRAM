37420273d71ea24631c37444d08b8c4ac208125e
CbazJmz <iesebastianjimenez@gmail.com>
2026-01-29 21:08:58 +0000

    Modified list.f file

--- verif/work/list.f
+++ verif/work/list.f
@@ -5,7 +5,9 @@
 #	../../rtl/sense_amp.sv
 #       ../../rtl/inv.sv
         ../../rtl/nmosfet.sv
+        ../../rtl/r_sram_cell.sv
 
 # ============================ verification
 #	../sram_cell_tb.sv
-        ../nmosfet_tb.sv
+#        ../nmosfet_tb.sv
+        ../r_sram_cell_tb.sv
