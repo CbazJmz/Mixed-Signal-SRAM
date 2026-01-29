6a7f26087861ce3472c12568fb9490a2ab4b6d13
CbazJmz <iesebastianjimenez@gmail.com>
2026-01-29 20:02:20 +0000

    Modified list.f file

--- verif/work/list.f
+++ verif/work/list.f
@@ -1,9 +1,9 @@
 # ============================ rtl
-	../../rtl/sram_cell.sv
-	../../rtl/write_driver.sv
+#	../../rtl/sram_cell.sv
+#	../../rtl/write_driver.sv
 #	../../rtl/precharge.sv
-	../../rtl/sense_amp.sv
-
+#	../../rtl/sense_amp.sv
+        ../../rtl/inv.sv
 
 # ============================ verification
-	../sram_cell_tb.sv
+#	../sram_cell_tb.sv
