49c896ee37d62bfd7ae62530f9a90e1a3e7b6a07
CbazJmz <iesebastianjimenez@gmail.com>
2026-02-03 18:32:48 +0000

    Modified sram_cell tb and tested in Xcelium

--- rtl/cell_array.sv
+++ rtl/cell_array.sv
@@ -1,11 +1,11 @@
 module cell_array #(
-    parameter ROWS=16, COLS=8)(
-    input  real row_wr [0:ROWS-1],
+   	parameter ROWS=16, COLS=8)(
+   	input  real row_wr [0:ROWS-1],
 	input  real row_rd [0:ROWS-1],
-    input  real bl_wr [0:COLS-1],
-    input  real blb_wr [0:COLS-1],
+   	input  real bl_wr [0:COLS-1],
+   	input  real blb_wr [0:COLS-1],
 	output  real bl_rd [0:COLS-1],
-    output  real blb_rd [0:COLS-1]
+   	output  real blb_rd [0:COLS-1]
 );
 
 //   ________________________________
@@ -25,12 +25,12 @@ const real VTH =  0.8;
         for(r=0;r<ROWS;r++) begin: ROW
             for(c=0;c<COLS;c++) begin: COL
                 sram_cell cell1(
-                    .r_row_wr (row_wr[r]),
-					.r_row_rd (row_rd[r]),
-                    .r_bl_wr (bl_wr[c]),
-                    .r_blb_wr(blb_wr[c]),
-					.r_bl_rd (bl_rd[c]),
-                    .r_blb_rd(blb_rd[c])
+                	.row_wr (row_wr[r]),
+			.row_rd (row_rd[r]),
+                    	.bl_wr (bl_wr[c]),
+                    	.blb_wr(blb_wr[c]),
+			.bl_rd (bl_rd[c]),
+                   	.blb_rd(blb_rd[c])
                 );
             end
         end
--- verif/r_sram_cell_tb.sv
+++ verif/r_sram_cell_tb.sv
@@ -3,8 +3,8 @@ module r_sram_cell_tb #(
 	
 	real row_wr [0:ROWS-1];
 	real row_rd [0:ROWS-1];
-    real bl_wr [0:COLS-1];
-    real blb_wr [0:COLS-1];
+    	real bl_wr [0:COLS-1];
+    	real blb_wr [0:COLS-1];
 	real bl_rd [0:COLS-1];
 	real blb_rd [0:COLS-1];
 
@@ -18,56 +18,56 @@ const real VDD =  1.5;
 const real VSS =  0.0;
 const real VTH =  0.8; 
 	
-    cell_array cell1 #(
-    .ROWS (ROWS),
-	.COLS (COLS))(
-    .row_wr (row_wr),
+    cell_array #(
+   	.ROWS (ROWS),
+	.COLS (COLS)) cell1(
+    	.row_wr (row_wr),
 	.row_rd (row_rd),
-    .bl_wr (bl_wr),
-    .blb_wr(blb_wr),
+    	.bl_wr (bl_wr),
+    	.blb_wr(blb_wr),
 	.bl_rd (bl_rd),
-    .blb_rd(blb_rd)
+    	.blb_rd(blb_rd)
     );
 	
     initial begin
 	
-		row_wr = 1'b0;
-		row_rd = 1'b0;
-		bl_wr = 0;
-		blb_wr = 0;
+		row_wr [0]= VSS;
+		row_rd [0]= VSS;
+		bl_wr [0]= VSS;
+		blb_wr [0]= VSS;
 		
 		#1ns;
 		
 		//Write 1 operation
 		
-		bl_wr = VDD;
-		blb_wr = VSS;
-		row_wr = 1'b1;
+		bl_wr [0]= VDD;
+		blb_wr [0]= VSS;
+		row_wr [0]= VDD;
 		#10ns;
-		row_wr = 1'b0;
+		row_wr [0]= VSS;
 		#10ns;
 		
 		//Read operation
 		
-		row_rd = 1'b1;
+		row_rd [0]= VDD;
 		#10ns;
-		row_rd = 1'b0;
+		row_rd [0]= VSS;
 		#10ns;
 		
 		//Write 0 operation
 		
-		bl_wr = VSS;
-		blb_wr = VDD;
-		row_wr = 1'b1;
+		bl_wr [0]= VSS;
+		blb_wr [0]= VDD;
+		row_wr [0]= VDD;
 		#10ns;
-		row_wr = 1'b0;
+		row_wr [0]= VSS;
 		#10ns;
 		
 		//Read operation
 		
-		row_rd = 1'b1;
+		row_rd [0]= VDD;
 		#10ns;
-		row_rd = 1'b0;
+		row_rd [0]= VSS;
 		#10ns;
 
         $finish;
