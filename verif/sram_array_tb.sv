2cced5a6632c8f30b919311d19ff87fc1acc0197
CbazJmz <iesebastianjimenez@gmail.com>
2026-02-03 16:09:20 -0600

    Fixed identations in tb file

--- verif/sram_array_tb.sv
+++ verif/sram_array_tb.sv
@@ -1,5 +1,5 @@
 module sram_array_tb #(
-    parameter ROWS=2, COLS=8)();
+	parameter ROWS=2, COLS=8)();
 	
 	logic [COLS-1:0] data;
 	real data_in [0:COLS-1];
@@ -8,8 +8,8 @@ module sram_array_tb #(
 	real row_rd [0:ROWS-1];
 	real bl_wr [0:COLS-1];
 	real blb_wr [0:COLS-1];
-	logic [COLS-1:0] bl_rd;
-	logic [COLS-1:0] blb_rd;
+	real bl_rd [0:COLS-1];
+	real blb_rd [0:COLS-1];
 
 //   ________________________________
 //  |              VDD   |    VSS    |
@@ -19,39 +19,39 @@ module sram_array_tb #(
 
 const real VDD =  1.5;
 const real VSS =  0.0;
-const real VTH =  0.8; 
-	
+const real VTH =  0.8;
+
 	write_driver #(
 		.COLS (COLS)) wr1(
-    	.data_in (data_in),
+		.data_in (data_in),
 		.bl_wr (bl_wr),
-    	.blb_wr(blb_wr)
-    );
+		.blb_wr(blb_wr)
+	);
 	
-    cell_array #(
+	cell_array #(
 		.ROWS (ROWS),
 		.COLS (COLS)) cell1(
-    	.row_wr (row_wr),
+		.row_wr (row_wr),
 		.row_rd (row_rd),
-    	.bl_wr (bl_wr),
-    	.blb_wr(blb_wr),
+		.bl_wr (bl_wr),
+		.blb_wr(blb_wr),
 		.bl_rd (bl_rd),
-    	.blb_rd(blb_rd)
-    );
+		.blb_rd(blb_rd)
+	);
 	
 	sense_amp #(
 		.COLS (COLS)) sa1(
-    	.preout (preout),
+		.preout (preout),
 		.bl_rd (bl_rd),
-    	.blb_rd(blb_rd)
-    );
+		.blb_rd(blb_rd)
+	);
 
 	genvar c;
 	for (c=0;c<COLS;c++) begin: COL
-	    assign data_in [c] = data [c] ? VDD : VSS;
+		assign data_in [c] = data [c] ? VDD : VSS;
 	end
 	
-    initial begin
+	initial begin
 	
 		row_wr [0]= VSS;
 		row_rd [0]= VSS;
@@ -71,13 +71,13 @@ const real VTH =  0.8;
 		row_rd [0]= VSS;
 		#10ns;
 
-        $finish;
-    end
+		$finish;
+	end
 
-    initial begin
-	    $shm_open("shm_db");
-	    $shm_probe("ASMTR");
-    end
+	initial begin
+		$shm_open("shm_db");
+		$shm_probe("ASMTR");
+	end
 
 endmodule
 
