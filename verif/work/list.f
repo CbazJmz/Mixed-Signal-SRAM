3a67f29e23503d8a557e368ab3fd736f3cb21233
CbazJmz <iesebastianjimenez@gmail.com>
2026-02-10 17:02:01 -0600

    Modified list.f

---
+++ verif/work/list.f
@@ -0,0 +1,17 @@
+#Defines
+#../../rtl/defines.svh
+
+#FV
+
+#RTL
+../../rtl/nmosfet.sv
+../../rtl/sram_cell.sv
+../../rtl/write_driver.sv
+../../rtl/decoder.sv
+../../rtl/sense_amp.sv
+../../rtl/cell_array.sv
+
+#TB
+#../nmosfet.sv
+#../sram_cell_tb.sv
+#../sram_array_tb.sv
