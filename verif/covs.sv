33342796e62f16706be98fa9c68f5d12a672c100
CbazJmz <iesebastianjimenez@gmail.com>
2026-02-13 12:47:00 -0600

    Added interface

---
+++ verif/covs.sv
@@ -0,0 +1,38 @@
+module cov_sram (
+	input logic clk,
+	input logic arst_n,
+
+	input logic serial_in;
+	input logic shift;
+	input logic w_en;
+	input logic r_en;
+	input logic [$clog2(ROWS)-1:0]addr;
+	input logic data_valid;
+	input logic [COLS-1:0]data_out;
+);
+
+	`define SRAM_PATH sram1
+
+	covergroup wr_port_cg @(posedge clk);
+		option.per_instance = 1;
+
+		wren_change: coverpoint w_en {
+		bins rise = (0 => 1);
+		bins fall = (1 => 0);
+		}
+		wren_value: coverpoint w_en { bins value[] = {0, 1}; }
+
+	endgroup: wr_port_cg
+
+	covergroup rd_port_cg @(posedge clk);
+		option.per_instance = 1;
+
+		rden_change: coverpoint r_en {
+		bins rise = (0 => 1);
+		bins fall = (1 => 0);
+		}
+		rden_value: coverpoint r_en { bins value[] = {0, 1}; }
+
+	endgroup: rd_port_cg
+
+endmodule: cov_sfifo
--- verif/sram_top_tb.sv
+++ verif/sram_top_tb.sv
@@ -13,15 +13,19 @@ module sram_top_tb;
 	sram_top sram1(
 	.clk (clk),
 	.arst_n (arst_n),
-	.serial_in (serial_in),
-	.w_en (w_en),
-	.r_en (r_en),
-	.shift (shift),
-	.addr (addr),
-	.data_valid (data_valid),
-	.data_out (data_out)
+	.serial_in (int1.serial_in),
+	.w_en (int1.w_en),
+	.r_en (int1.r_en),
+	.shift (int1.shift),
+	.addr (int1.addr),
+	.data_valid (int1.data_valid),
+	.data_out (int1.data_out)
 	);
 
+	sram_top_intf int1(
+	.clk (clk),
+	.arst_n (arst_n)
+	);
 	// Clock generation
 	always #5 clk = ~clk;
 
@@ -40,37 +44,14 @@ module sram_top_tb;
 
 	// Test procedure
 	initial begin
-		clk = 1'b0;
-		arst_n = 1'b0;
-		serial_in = 1'b0;
-		shift = 1'b0;
-		w_en = 1'b0;
-		r_en = 1'b0;
-		addr = '0;
+		//initialize
+		int1.initialize();
 
-		// Reset
-		#20;
-		arst_n = 1'b1;
-
-		// ----------------------
-		// WRITE TEST
-		// ----------------------
-		addr = 1'b1;
+		// write test
+		int1.full_write();
 		
-		send_serial_word(8'b10011101);
-
-		@(posedge clk);
-		w_en = 1'b1;
-		@(posedge clk);
-		w_en = 1'b0;
-
-		// ----------------------
-		// READ TEST
-		// ----------------------
-		@(posedge clk);
-		r_en = 1'b1;
-		@(posedge clk);
-		r_en = 1'b0;
+		// read test
+		int1.full_read ();
 
 		#30;
 		$finish;
@@ -81,5 +62,7 @@ module sram_top_tb;
 		$shm_open("shm_db");
 		$shm_probe("ASMTR");
 	end
+
+	bind sram_top cov_sram cov1 (.*);
 
 endmodule
